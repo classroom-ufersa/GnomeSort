e81bef559114decc4f4662306ece8653d1f44114
deborabckp <debora.costa22396@alunos.ufersa.edu.br>
2023-03-27 23:38:21 -0300

    Criação

---
+++ aluno.h
@@ -0,0 +1,9 @@
+typedef union docs Documentos;
+ 
+typedef struct aluno Alunos;
+ 
+void ler_dados(Alunos **vetor_aluno, int quantidade_alunos);
+ 
+void gnomeSort(Alunos **vetor_aluno, int quantidade_alunos);
+ 
+void arquivo(Alunos **vetor_aluno, int quantidade_alunos);
