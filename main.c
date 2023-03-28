3ff61669037186b1eb388cdb6a69d8929ff16c72
FernandaRFs <fernandarochafernandes95@gmail.com>
2023-03-28 14:48:00 -0300

    main

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "aluno.c"
 
 
-int main(void){
 
-    printf("Hello World!");
-
+int main (void){
+    int qnt, i;
+    printf("Informe a quantidade: ");
+    scanf("%d", &qnt);
+    getchar();
+    Alunos **vetor = (Alunos **) malloc (qnt*sizeof(Alunos*));
+    for(i=0;i<qnt;i++){
+        vetor[i]=(Alunos*)malloc(MAX_ALUNO*sizeof(Alunos));
+    }
+    ler_dados(vetor, qnt);
+    gnomeSort(vetor, qnt);
+    arquivo(vetor, qnt);
+    for(i = 0; i < qnt; i++){
+        free(vetor[i]);
+    }
+    free(vetor);
     return 0;
 }
