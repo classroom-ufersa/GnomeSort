2ea2829fba5c3ab807bd0e2d91746c9a2b5b6bd4
Armandoedu <armandoeduardo306@gmail.com>
2023-03-29 00:34:54 -0300

    main

--- main.c
+++ main.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "aluno.c"
-
-
-
+ 
 int main (void){
-    int qnt, i;
-    printf("Informe a quantidade: ");
-    scanf("%d", &qnt);
+    int quantidade_alunos, index;
+    size_t limpar;
+    printf("Informe a quantidade de alunos que serão inscritos: \t");
+    scanf("%d", &quantidade_alunos);
+    printf("\n");
     getchar();
-    Alunos **vetor = (Alunos **) malloc (qnt*sizeof(Alunos*));
-    for(i=0;i<qnt;i++){
-        vetor[i]=(Alunos*)malloc(MAX_ALUNO*sizeof(Alunos));
+    Alunos **vetor_aluno = (Alunos **) malloc (quantidade_alunos*sizeof(Alunos*));
+    if (vetor_aluno == NULL) {
+        printf("Erro ao alocar memoria.\n");
+        exit(1);
     }
-    ler_dados(vetor, qnt);
-    gnomeSort(vetor, qnt);
-    arquivo(vetor, qnt);
-    for(i = 0; i < qnt; i++){
-        free(vetor[i]);
+    for(index = 0; index < quantidade_alunos; index++){
+        vetor_aluno[index]=(Alunos*)malloc(MAX_ALUNO*sizeof(Alunos));
+        if (vetor_aluno[index] == NULL) {
+            printf("Erro ao alocar memoria.\n");
+            exit(1);
+        }
     }
-    free(vetor);
-    return 0;
+    printf("Deseja criar um novo arquivo para inserir os dados dos alunos?\n");
+    printf("1-SIM\t\t\t\t2-NÃO\n\t\t");
+    scanf("%li", &limpar);
+    getchar();
+    printf("\n\n");
+    if(limpar == 1){
+        limpar_arquivo();
+        printf("\t\t\t\tInsira os dados dos alunos\n");
+    } else if(limpar == 2){
+        printf("\t\t\t\tInsira os dados dos alunos\n");
+    } else{
+        printf("Opção inválida!");
+        exit(1);
+    }
+    printf("\n\n\n");
+    ler_dados(vetor_aluno, quantidade_alunos);
+    gnomeSort(vetor_aluno, quantidade_alunos);
+    arquivo(vetor_aluno, quantidade_alunos);
+    for(index = 0; index < quantidade_alunos; index++){
+        free(vetor_aluno[index]);
+    }
+    free(vetor_aluno);
+    return(0);
 }
