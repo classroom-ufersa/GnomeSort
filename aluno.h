2a0632d056baa6b174e4ab16b616df4a36285f6c
angellusj <angela.souza@alunos.ufersa.edu.br>
2023-03-29 03:15:48 +0000

    updated by angela

--- aluno.h
+++ aluno.h
@@ -1,9 +1,11 @@
 typedef union docs Documentos;
- 
+
 typedef struct aluno Alunos;
- 
+
 void ler_dados(Alunos **vetor_aluno, int quantidade_alunos);
- 
+
 void gnomeSort(Alunos **vetor_aluno, int quantidade_alunos);
- 
+
+void limpar_arquivo();
+
 void arquivo(Alunos **vetor_aluno, int quantidade_alunos);
